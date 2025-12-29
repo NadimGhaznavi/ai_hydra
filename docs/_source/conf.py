b34def3413bb24f9f32a77a40c6303bf5e09716e
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2025-12-29 17:46:04 -0500

    chore: Update project version to 0.4.6
    - Update version in pyproject.toml from 0.4.5 to 0.4.6
    - Update version in ai_hydra/__init__.py to 0.4.6
    - Update version in ai_hydra/tui/__init__.py to 0.4.6
    - Update version in docs/_source/conf.py to 0.4.6
    
    All version references now consistently point to 0.4.6 for the release
    with ReadTheDocs configuration fixes.

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -33,8 +33,8 @@ copyright = '2024, AI Hydra Team'
 author = 'AI Hydra Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.5'
-version = '0.4.5'
+release = '0.4.6'
+version = '0.4.6'
 
 # -- General configuration ---------------------------------------------------
 
