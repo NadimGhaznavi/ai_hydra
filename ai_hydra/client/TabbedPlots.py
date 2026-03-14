9850264f00ffb1a130a1e117debf43a8e14be86e
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-03-13 23:13:20 -0400

    Style: Moved current plot legend

--- ai_hydra/client/TabbedPlots.py
+++ ai_hydra/client/TabbedPlots.py
@@ -274,7 +274,7 @@ class TabbedPlots(Widget):
         plot.set_ylimits()
         plot.set_xlabel(DLabel.EPISODES)
         plot.set_ylabel(DLabel.LOSS)
-        plot.show_legend(location=LegendLocation.TOPRIGHT)
+        plot.show_legend(location=LegendLocation.TOPLEFT)
 
     def _plot_scatter_scores(self) -> None:
         plot = self.query_one(f"#{DField.SCORES_SCATTER_PLOT}", PlotWidget)
