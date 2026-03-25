0574f968fb35e6ef9b859ca38a035942bf2b70c3
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-03-25 17:19:00 -0400

    Feat: Update with path to QA env

--- scripts/qasync.sh
+++ scripts/qasync.sh
@@ -2,8 +2,15 @@
 #
 
 SRC=ai_hydra
-DEST=/opt/dev/ai_hydra/hydra_venv/lib/python3.11/site-packages/ai_hydra
+DEV_DEST=/opt/dev/ai_hydra/hydra_venv/lib/python3.11/site-packages/ai_hydra
+QA_DEST=/opt/qa/ai_hydra/hydra_venv/lib/python3.11/site-packages/ai_hydra
 
-rm -rf $DEST
-mkdir $DEST
-rsync -avr --delete $SRC/* $DEST
+
+rm -rf $DEV_DEST
+mkdir $DEV_DEST
+rsync -avr --delete $SRC/* $DEV_DEST
+
+
+rm -rf $QA_DEST
+mkdir $QA_DEST
+rsync -avr --delete $SRC/* $QA_DEST
