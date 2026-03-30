ea4942c5f21d3ec9382d44057a1a2f103f3ce7ae
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-03-30 17:47:49 -0400

    Feat: GRU tuning

--- ai_hydra/constants/DNNet.py
+++ ai_hydra/constants/DNNet.py
@@ -36,7 +36,7 @@ class DGRU:
     MAX_STAGNANT_EPISODES: Final[int] = 300
     MAX_TRAINING_FRAMES: Final[int] = 512
     MINIMUM_EPSILON: Final[float] = 0.0
-    NICE_P_VALUE: Final[float] = 0.001  # 0.005
+    NICE_P_VALUE: Final[float] = 0.01
     NICE_STEPS: Final[int] = 20
     NUM_COOLDOWN_EPISODES: Final[int] = 200
     OUTPUT_SIZE: Final[int] = 3
