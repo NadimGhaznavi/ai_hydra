4cfe4198e32d1e3c252d2d86dfdb0275529608d9
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-04-04 12:59:17 -0400

    v0.25.2 - Clear Docs Release

--- ai_hydra/constants/DHydra.py
+++ ai_hydra/constants/DHydra.py
@@ -22,7 +22,7 @@ class DHydra:
     NETWORK_TIMEOUT: Final[float] = 2.0
     PROTOCOL_VERSION: Final[int] = 1
     RANDOM_SEED: Final[int] = 1970
-    VERSION: Final[str] = "0.25.1"
+    VERSION: Final[str] = "0.25.2"
     HYDRA_DIR: Final[str] = "AI-Hydra"
 
 
