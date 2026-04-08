b65a07422ceedf4faa774293598d7f9e207cdc78
Nadim-Daniel Ghaznavi <nghaznavi@gmail.com>
2026-04-08 19:31:12 -0400

    Feat: Filtered out pycache from rsync output

--- scripts/devsync.sh
+++ scripts/devsync.sh
@@ -6,4 +6,4 @@ DEV_DEST="/opt/dev/ai_hydra/hydra_venv/lib/python3.11/site-packages/ai_hydra/"
 
 mkdir -p "$DEV_DEST"
 
-rsync -avr --delete "$SRC" "$DEV_DEST"
+rsync -avr --delete "$SRC" "$DEV_DEST" | grep -v __pycache__
--- scripts/qasync.sh
+++ scripts/qasync.sh
@@ -6,4 +6,4 @@ QA_DEST="/opt/qa/ai_hydra/hydra_venv/lib/python3.11/site-packages/ai_hydra/"
 
 mkdir -p "$QA_DEST"
 
-rsync -avr --delete "$SRC" "$QA_DEST"
+rsync -avr --delete "$SRC" "$QA_DEST" | grep -v __pycache__
